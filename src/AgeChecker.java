3380ef1dc85d27a173634077e682b76e75baca3e
Marek Smialowsky <marek.smialowsky@gmail.com>
2023-01-26 08:30:19 +0100

    dodanie AgeChecker

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -5,7 +5,7 @@ public class AgeChecker {
         System.out.println("Proszę podaj swój wiek");
         Scanner scanner = new Scanner(System.in);
         int wiek = scanner.nextInt();
-        if (wiek < 18 && wiek > 0) {
+        if (wiek < 18 && wiek >= 0) {
             System.out.println("Przykro mi nie mogę sprzedać Tobie alkoholu");
         } else if(wiek < 0) {
             System.out.println("Podałeś niepoprawną wartość");
