0702866e19736657de4a457b3f6aefb44367ad7f
Marek Smialowsky <marek.smialowsky@gmail.com>
2023-01-30 08:04:46 +0100

    kolejne klasy

--- src/While.java
+++ src/While.java
@@ -2,7 +2,7 @@ public class While {
     public static void main(String[] args) {
         int number = 0;
         while (number < 20){
-            System.out.println("Moja wartość to: " + number + (" i jestem mniejsza od 20"));
+            System.out.println("Moja wartość to: " + number + " i jestem mniejsza od 20");
             number++;
         }
     }
